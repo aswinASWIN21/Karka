02ebf8d03b451125c3a48d044b1243eabb7300ed
aswinASWIN21 <bunkyhunky1234@gmail.com>
2025-06-11 15:50:16 +0530

    task1

---
+++ trail/task1/task1.js
@@ -0,0 +1,81 @@
+//  task1,
+// console.log("hello world")
+
+//task2
+// console.log("aswin")
+
+// task3
+// let name = ("aswin")
+// let age = (22)
+// console.log("my name is",name)
+// console.log("my age is",age)
+
+// task 4
+// let name = ("aswin")
+// let age = (22)
+// let weight = (44.5)
+// let marital_status = ("NO")
+// console.log("my name is",name)
+// console.log("my age is",age)
+// console.log("my weight is",weight)
+// console.log("my marital_status is",marital_status)
+
+// task5,6
+// let name = ("aswin")
+// let age = (22)
+// let weight = (44.5)
+// let marital_status = ("NO")
+// console.log("my name is",name , "my age is", age, "my weight is",weight,"my marital_status is",marital_status)
+
+// task7 
+// console.log("*")
+// console.log("**")
+// console.log("***")
+// console.log("****")
+
+// task8
+
+// let num1 = 20
+// let num2 = 5
+// let addition = num1 + num2
+// let subtraction = num1 - num2
+// let multiplication = num1 * num2
+// let division = num1 / num2
+// console.log("Addition:", addition)         
+// console.log("Subtraction:", subtraction)    
+// console.log("Multiplication:", multiplication)
+// console.log("Division:", division)          
+
+//task9
+
+// let name = "Aswin";
+// let age = 24;
+// let isStudent = true;
+// console.log("Name:", name);
+// console.log("Age:", age);
+// console.log("Is Student:", isStudent);
+
+// name = "rishi";
+// age = age + 1;                 
+// isStudent = !isStudent;        
+// console.log("Updated Name:", name);
+// console.log("Updated Age:", age);
+// console.log("Updated Is Student:", isStudent);
+
+//task10
+
+// let name = "Aswin"
+// let age = 24
+// let dob = "2000-05-15"       
+// let height = 5.9;               
+// let weight = 68.5;              
+// let degree = "B.Tech in Computer Science"
+// let gender = "Male"
+// console.log("Name:", name)
+// console.log("Age:", age)
+// console.log("Date of Birth:", dob)
+// console.log("Height:", height, "ft")
+// console.log("Weight:", weight, "kg")
+// console.log("Degree:", degree)
+// console.log("Gender:", gender)
+
