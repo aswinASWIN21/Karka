ee311d9830bb90e2ede65ebd95db20d0b895f779
aswinASWIN21 <bunkyhunky1234@gmail.com>
2025-06-11 15:33:03 +0530

    task5

---
+++ trail/task5/task5.js
@@ -0,0 +1,241 @@
+ // task1
+
+// for(let i =0;i<10;i++){
+//  console.log(i)
+//     }
+
+// task2,3
+
+// let even = []
+// let odd = []
+// for(let i =0;i<20;i++){
+//     if([i]%2==0){
+//         even.push(i)
+//     }
+//     else{
+//         odd.push(i)
+//     }
+// }
+// console.log(even)
+// console.log(odd)
+
+// task4
+
+//  let num = 5
+//  let factorial = 1
+//  for(let i = 1; i <= num; i++) {
+//      factorial = factorial * i
+//  }
+//  console.log(factorial)
+
+
+// task5
+
+// let sum=0
+// for(let i =1;i<=100;i++){
+//     sum = sum+i
+// }
+// console.log(sum)
+
+// task6
+//  let a=[10,20,30,40,50,60]
+//  let sum = 0
+//  for(let i = 0;i<a.length;i++){
+//     sum = (sum+a[i])
+//  }
+//  let average = sum/a.length
+//  console.log(average)
+
+// task7
+// let num=5
+// let num1=10
+
+// for(i=0;i<num;i++){
+//     row=""
+//     for(j=0;j<num1;j++){
+//         row+="*"
+        
+//     }console.log(row)
+// }
+
+// let num=5
+
+// for(i=0;i<num;i++){
+//     row=""
+//     for(j=0;j<num;j++){
+//         row+="*"
+        
+//     }console.log(row)
+// }
+
+// let num=5
+
+// for(i=0;i<=num;i++){
+//     row=""
+//     for(j=0;j<5;j++){
+//         row+="*"   
+//     }console.log(row)
+// }
+
+// let rows = 4;
+
+// for (let i = 1; i <= rows; i++) {
+//   let row = '';
+
+//   for (let j = 0; j < rows - i; j++) {
+//     row += ' ';
+//   }
+
+//   for (let k = 0; k < 2 * i - 1; k++) {
+//     row += '*';
+//   }
+
+//   console.log(row);
+// }
+
+// task8
+
+// for(let i =0;i<5;i++){
+//  console.log(i)
+//     }
+
+// task9
+
+// for(let i =1;i<10;i++){
+//  console.log(i)
+//     }
+
+
+// task10
+
+// let a = [10, 20, 30, 40, 10]; 
+
+// if (a[0] === a[a.length - 1]) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+
+// task11
+
+// let a = [50,60,70,10,22,33]
+// for(let i=0;i<a.length;i++){
+//     if(a[i]%5==0){
+//         console.log(a[i])
+        
+//     }
+
+// }
+
+// task12
+
+// let char = "a"
+// if (char==["a,e,i,o,u"]){
+//     console.log("character is vovwel")
+// }
+// else{
+//     console.log("character is not vovwel")
+// }
+
+// tsk13
+
+// let even = []
+// let odd = []
+// for(let i =10;i<=55;i++){
+//     if([i]%2==0){
+//         even.push(i)
+//     }
+//     else{
+//         odd.push(i)
+//     }
+// }
+// console.log(even.length)
+// console.log(odd.length)
+
+
+// task14
+
+// for(let i =0;i<25;i++){
+//     if(i%5!==0){
+//         console.log(i)
+//     }
+    
+//     }
+
+//task15
+
+// let numbers = [3, 5, 0, 2];  // You can change or add numbers here
+
+// for (let i = 0; i < numbers.length; i++) {
+//     let num = numbers[i];
+//     let factorial = 1;
+
+//     for (let j = 1; j <= num; j++) {
+//         factorial *= j;
+//     }
+
+//     console.log("Factorial of", num, "is", factorial);
+// }
+
+
+
+// task16 
+
+// function productorsum(a,b){
+//     let product = a*b
+//     let sum = a+b
+//     if (product >500){
+//         return sum
+//     }
+// else{
+//     return product
+// }}
+// console.log(productorsum(20,10))
+
+// task17
+
+// let a = 40
+// let b = 20
+// if(a>b){
+//     console.log("a is greatest")
+// }
+// else{
+//     console.log("b is greatest")
+// }
+
+
+
+// task18
+
+// let a = 12
+// let b = 130
+// let c = 20
+// if(a>=b&&a>c){
+//     console.log("a is greatest")
+// }
+// else if(b>=a&&b>c){
+//     console.log("b is greatest")
+// }
+// else{
+//     console.log("c is greatest")
+// }
+
+// task19
+// let x = [23,4,-6,23,-9,21,3,-45,-8]
+
+// let positive = []
+// let negative= []
+
+// for (let i = 0; i < x.length; i++) {
+//     if (x[i] >= 0) {
+//         positive.push(x[i])
+//     } else {
+//         negative.push(x[i])
+//     }
+// }
+
+// console.log("positive numbers:", positive);
+// console.log("negative numbers:", negative);
+
+
