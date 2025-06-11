1a4003a84e33eca5c21a1c601d646348ad2719ea
aswinASWIN21 <bunkyhunky1234@gmail.com>
2025-06-11 15:14:31 +0530

    task4

---
+++ trail/task4/task4.js
@@ -0,0 +1,158 @@
+// task1
+
+// let a = -11
+// if(a>0){
+//     console.log("the number is positive")
+// }
+// else{
+//     console.log("the number is negative")
+// }
+
+// task2
+
+// let a = 11
+// if(a%2==0){
+//     console.log("number is even")
+// }
+// else{
+//     console.log("number is odd")
+// }
+
+// task3
+
+// let base = 5
+// let exponent = 2
+
+// let result = base ** exponent;
+
+// console.log(result)
+
+// task4
+
+// let a =5
+// let b = 10
+// if(a>b){
+//     console.log("a is greatest")
+// }
+// else{
+//     console.log("b is greatest")
+// }
+
+// task5
+
+// let year = 2024
+
+// if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//     console.log(year + " is a Leap Year.")
+// } else {
+//     console.log(year + " is NOT a Leap Year.")
+// }
+
+
+// task6
+
+// let marks = 200
+// if(marks>=450){
+//     console.log("A+")
+// }
+// else if(marks>=400)
+// {
+//     console.log("A")
+// }
+// else if(marks>=350)
+// {
+//     console.log("B+")
+// }
+// else if(marks>=300)
+// {
+//     console.log("B")
+// }
+// else if(marks>=250)
+// {
+//     console.log("c")
+// }
+// else(marks<=250)
+// {
+//     console.log("fail")
+// }
+
+// task7
+
+// let age = 24
+// if(age<=16){
+//     console.log("You can't drive")
+// }
+// else if(age>=16&age<=17){
+//     console.log("You can drive but not vote")
+// }
+// else if(age>=18&age<=24){
+//     console.log("You can vote but not rent a car")
+// }
+// else if(age>=25)
+// {
+//     console.log("You can do pretty much anything")
+
+// }
+
+// task8
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 ===0){
+//         console.log("FizzBuzz")
+//     }
+//        else  if(i%3){
+//         console.log("Fizz")
+//     }
+//     else if(i%5){
+//         console.log("Buzz")
+
+//     }}
+
+//  task9
+
+// let year = 2024
+// if((year%4==0&&year%100!==0)||year%400==0){
+//     console.log("its leap year")}
+//     else { console.log("not leap year")
+
+//     }
+
+
+
+// let a = [10,21,31,40,50]
+// for(let i=0;i<a.length;i++){
+//     if(i%2==0){
+//         console.log("even",a[i])
+//     }
+//     else{
+//         console.log("odd,",a[i])
+//     }   
+// }
+
+// let a = [10,21,31,40,50,60]
+// let odd = []
+// let even = []
+// for(let i=0;i<a.length;i++){
+//     if(a[i]%2==0){
+//         even.push(a[i])
+//     }
+//     else{
+//         odd.push(a[i])
+//     }}
+// console.log(even)
+// console.log(odd)
+
+// let even = []
+// let odd = []
+// for(let i =0;i<100;i++){
+//     if([i]%2==0){
+//         even.push(i)
+//     }
+//     else{
+//         odd.push(i)
+//     }
+// }
+// console.log(even)
+// console.log(odd)
+
+
