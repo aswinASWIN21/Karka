4929c34f791c7f7cba122aa9fc15c7cb81b3af53
aswinASWIN21 <bunkyhunky1234@gmail.com>
2025-06-11 10:45:07 +0530

    Task1

---
+++ trail/task1/task1.js
@@ -0,0 +1,183 @@
+// task 2
+
+let a = 10
+let b = 20
+let c = a+b
+console.log(c)
+
+// task3
+
+// function mul(pi,r,r){
+//     let areaofcircle = pi*r*r
+//     console.log(areaofcircle)
+
+// }
+// mul(3.14,10,10)
+
+// task4
+
+// function mul(l,w){
+//     let areaofrectangle = l*w
+//     console.log(areaofrectangle)
+// }
+// mul(10,10)
+
+// task5
+
+// function mul(b,h){
+//     let areaoftriangle = b*h/2
+//     console.log(areaoftriangle)
+// }
+// mul(10,10)
+
+// task6,7
+
+// let a = 10
+// let b = 20
+// let c = a+b
+// console.log(c)
+
+// let a = 10
+// let b = 20
+// let c = a*b
+// console.log(c)
+
+// let a = 10
+// let b = 20
+// let c = a/b
+// console.log(c)
+
+// let a = 10
+// let b = 20
+// let c = a%b
+// console.log(c)
+
+// let a = 10
+// let b = 20
+// let c = a-b
+// console.log(c)
+
+// task7
+
+// let a = 20
+// a += 100
+// console.log(a)
+
+// let a = 20
+// a -= 100
+// console.log(a)
+
+// let a = 20
+// a *= 100
+// console.log(a)
+
+// let a = 20
+// a /= 100
+// console.log(a)
+
+// let a = 20
+// a %= 100
+// console.log(a)
+
+// task9
+
+// let a = 10
+// let b = 11
+// console.log(a=b)
+
+// let a = 10
+// let b = 10
+// console.log(a==b)
+
+// let a = 10
+// let b = 10
+// console.log(a!=b)
+
+// let a = 10
+// let b = 10
+// console.log(a>b)
+
+// let a = 10
+// let b = 10
+// console.log(a>=b)
+
+// let a = 10
+// let b = 10
+// console.log(a<b)
+
+// let a = 10
+// let b = 10
+// console.log(a<=b)
+
+// task10
+
+// let a = 50
+// let b = 70
+// console.log(a<b && a>10)
+
+// let a = 50
+// let b = 70
+// console.log(a>100 && a>30)
+
+// let a = 50
+// let b = 70
+// console.log(a>30 && a>100)
+
+// let a = 50
+// let b = 70
+// console.log(a<30 && a>b)
+
+// let a = 50
+// let b = 70
+// console.log(a<b ||a>10)
+
+// let a = 50
+// let b = 70
+// console.log(a>100 ||a>30)
+
+// let a = 50
+// let b = 70
+// console.log(a>30 ||a>100)
+
+// let a = 50
+// let b = 70
+// console.log(a<30 ||a>b)
+
+// task11
+// let a = 10
+// let b = 20
+// let c = a
+// a = b
+// b = c
+// console.log("a=",a)
+// console.log("b=",b)
+
+// task12
+// let a = 10
+// let b = 10
+// let c = 10
+// let average = (a+b+c)/3
+// console.log(average)
+//task13
+// let a = 10
+// let b = 30
+// let c = 12
+// let d = 3
+// let compound = (a+b)
+// let compound2 = (a+b)*c
+// let compound3 = (a+b)*(c)/(d)
+// console.log(compound)
+// console.log(compound2)
+// console.log(compound3)
+
+// task14
+// let tamil = 70
+// let english = 80
+// let maths = 90
+// let science = 100
+// let social = 75
+// let total = (tamil+english+maths+science+social)
+// let average = ((tamil+english+maths+science+social)/5)
+// console.log("total marks",total)
+// console.log("average marks",average)
+
